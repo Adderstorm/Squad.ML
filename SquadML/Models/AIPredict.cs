5f8ac36264eeaca6f4842fb86de0cb6fec6f8706
Avriel <tractar313@gmail.com>
2023-06-04 23:59:45 +0600

    AI predict update

--- SquadML/Models/AIPredict.cs
+++ SquadML/Models/AIPredict.cs
@@ -1,7 +1,24 @@
-﻿namespace SquadML.Application.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SquadML.Application.Models
 {
+    [Table("AIPredict")]
     public class AIPredict
     {
 
+        [Column("totalMembers")]
+        public int TotalMembers { get; set; }
+
+        [Column("membersActivity")]
+        public int MembersActivity { get; set; }
+
+        [Column("newMembers")]
+        public int NewMembers { get; set; }
+
+        [Column("voiceActivity")]
+        public int VoiceActivity { get; set; }
+
+        [Column("messagesActivity")]
+        public int MessagesActivity { get; set; }
     }
 }
